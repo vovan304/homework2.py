36c685284cb7b9888727f382c3ec26ebe87963e5
vovan304 <sergiy_kolosov@mail.ru>
2024-07-06 23:04:29 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+numbers_of_homework = 12
+total_hours = 1.5
+name_of_course = 'Python'
+time_of_one_homework = total_hours/numbers_of_homework
+print('Курс:',name_of_course,', всего задач:',numbers_of_homework,', затрачено часов:',total_hours,', среднее время выполнения',time_of_one_homework, 'часа.')
